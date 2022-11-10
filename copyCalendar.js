05a21b4064fdbcde40f464e30d47ad66a7a623e8
ofekzil <ofekzil162@gmail.com>
2022-11-09 19:56:14 -0800

    Reads data from file and to JSON (no file yet)

--- copyCalendar.js
+++ copyCalendar.js
@@ -1,23 +1,63 @@
-var schedule = {
-  "Month" : "",
-  "Year" : "",
-  "Days" : [],
+// import { writeFile } from 'fs'; // find how to import file properly
 
-};
-
+// sets the schedule to proper JSON format
 function setSchedule() {
+  var schedule = {
+    "Month" : "",
+    "Year" : "",
+    "Days" : [],
+  
+  };
+  // monthYear is of teh form "Month Year" (w/ teh space)
   let monthYear = document.getElementsByClassName("pageTitle")[0].innerText;
-  let workDaysInfo = document.getElementsByClassName("calendarCellRegularFuture"); //use innerTest to access strings of the format 10\t17:00 - 23:00\n(CONCESSION, 11149)\nmore. here
-
+  const separated = monthYear.split(/\s+/);
+  console.assert(separated.length === 2);
+  schedule["Month"] = separated[0];
+  schedule["Year"] = separated[1];
+  console.log(schedule["Month"]);
+  console.log(schedule["Year"]);
+  //strings in workDaysInfo are of the form "Day Start_Time - End_Time\n(Job, Department)\nmore..."
+  let workDaysInfo = document.getElementsByClassName("calendarCellRegularFuture"); 
+  for (let i = 0; i < workDaysInfo.length; i += 2) {
+    let curr = workDaysInfo[i].innerText;
+    curr = curr.replace("more...", "").replace("-", "").replace(/,/g, "").replace("(","").replace(")","");
+    console.log(curr);
+    const parts = curr.split(/\s+/);
+    console.log(parts.length);
+    //console.assert(parts.length === 5);
+    schedule["Days"].push(setDay(parts));
+  }
+  console.log(schedule["Days"]); 
+  
+  writeToFile(schedule);
 }
 
+// returns a JSON representation of a day enty
+function setDay(dayInfo) {
+  var day = {
+    "Date": dayInfo[0],
+    "Start_Time": dayInfo[1],
+    "End_Time": dayInfo[2],
+    "Job": dayInfo[3],
+    "Department": dayInfo[4]
+  }
 
-
-for (let i = 0; i < workDaysInfo.length; i++) {
-  let curr = workDaysInfo[i].innerText;
-
-  console.log(curr);
+  return day;
 }
+
+// writes the complete schedule to file
+function writeToFile(obj) {
+  const data = JSON.stringify(schedule);
+  writeFile("./schedule.json", data, err => {
+    if (err) {
+      console.log("Error writing file", err)
+    } else {
+      console.log("successfuly wrote to file");
+    }
+  })
+}
+
+setSchedule();
 
 
 console.log("finished in copy")
--- saveCalendar.js
+++ saveCalendar.js
@@ -1,15 +1,17 @@
 
 
 chrome.tabs.onUpdated.addListener((tabID,tab) => {
-
+  if (tab.url && tab.url.includes("workbrain.cineplex.com/etm/time/timesheet/etmTnsMonth")) {
     chrome.tabs.query({ active: true }, function (tabs) {
-        chrome.scripting.executeScript({
-            target: { tabId: tabID },
-            files: ["copyCalendar.js"],
-        });
-        console.log("finished execute script")
+      chrome.scripting.executeScript({
+          target: { tabId: tabID },
+          files: ["copyCalendar.js"],
       });
+      console.log("finished execute script")
+    });
 
+  }
+    
         
     }); 
 
