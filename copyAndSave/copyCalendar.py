28faa124c5b509967e1ffbae51fab7fa2f8dbf6e
ofekzil <ofekzil162@gmail.com>
2022-11-16 18:53:20 -0800

    updated file finder conditional to use regex

--- copyAndSave/copyCalendar.py
+++ copyAndSave/copyCalendar.py
@@ -7,10 +7,62 @@ from pathlib import Path
 from ics import Calendar, Event
 from zoneinfo import ZoneInfo
 from datetime import datetime
+import re
 
 
 months = {"January": 1, "February": 2, "March": 3, "April": 4, "May": 5, "June": 6, 
 "July": 7, "August": 8, "September": 9, "October": 10, "November": 11, "December": 12}
+
+def main():
+    curr_sched = get_recent_schedule()
+    print(curr_sched.name)
+    curr_data = load_data(curr_sched)
+
+    print(curr_data)
+
+    curr_calendar = create_calendar(curr_data)
+    print(curr_calendar.serialize())
+
+    write_to_file(curr_calendar)
+
+# finds and returns the most recently downloaded schedule in the user's downloads folder
+def get_recent_schedule():
+    entries = os.scandir(get_download_folder())
+
+    schedules = list()
+
+    for entry in entries:
+        if (re.search("schedule(\s\(\d\))?\.json",entry.name) != None):
+            schedules.append(entry)
+
+    most_recent = max(schedules, key=os.path.getctime)
+    return most_recent
+
+# load json data from the file
+def load_data(sched_file):
+    file = open(sched_file)
+    data = json.load(file)
+    return data
+
+# creates .ics file from given schedule data
+def create_calendar(curr_data):
+    calendar = Calendar()
+    year = int(curr_data["Year"])
+    month = months[curr_data["Month"]]
+
+    for day in curr_data["Days"]:
+        begin = datetime(year, month, int(day["Date"]), int(day["Start_Time"][0:2]), minute=int(day["Start_Time"][3:5]), tzinfo=ZoneInfo("America/Vancouver"))
+        end = datetime(year, month, int(day["Date"]), int(day["End_Time"][0:2]), minute=int(day["End_Time"][3:5]), tzinfo=ZoneInfo("America/Vancouver"))
+        shift = Event(name=day["Job"], begin=begin, end=end, location=day["Department"])
+        calendar.events.add(shift)
+        print(begin)
+        print(end)
+    return calendar
+
+# writes the calendar to an ics file
+def write_to_file(calendar):
+    open("./copyAndSave/fullSchedule.ics", "w").writelines(calendar)    
+
 
 # accesses the user's downloads folder
 if os.name == 'nt':
@@ -57,55 +109,6 @@ else:
         home = os.path.expanduser("~")
         return os.path.join(home, "Downloads")
 
-# finds and returns the most recently downloaded schedule in the user's downloads folder
-def get_recent_schedule():
-    entries = os.scandir(get_download_folder())
-
-    schedules = list()
-
-    for entry in entries:
-        if (entry.is_file() and 'schedule' in entry.name and '.json' in entry.name):
-            schedules.append(entry)
-
-    most_recent = max(schedules, key=os.path.getctime)
-    return most_recent
-
-# load json data from the file
-def load_data(sched_file):
-    file = open(sched_file)
-    data = json.load(file)
-    return data
-
-# creates .ics file from given schedule data
-def create_calendar(curr_data):
-    calendar = Calendar()
-    year = int(curr_data["Year"])
-    month = months[curr_data["Month"]]
-
-    for day in curr_data["Days"]:
-        begin = datetime(year, month, int(day["Date"]), int(day["Start_Time"][0:2]), minute=int(day["Start_Time"][3:5]), tzinfo=ZoneInfo("America/Vancouver"))
-        end = datetime(year, month, int(day["Date"]), int(day["End_Time"][0:2]), minute=int(day["End_Time"][3:5]), tzinfo=ZoneInfo("America/Vancouver"))
-        shift = Event(name=day["Job"], begin=begin, end=end, location=day["Department"])
-        calendar.events.add(shift)
-        print(begin)
-        print(end)
-    return calendar
-
-# writes the calendar to an ics file
-def write_to_file(calendar):
-    open("./copyAndSave/fullSchedule.ics", "w").writelines(calendar)    
-
-def main():
-    curr_sched = get_recent_schedule()
-    print(curr_sched.name)
-    curr_data = load_data(curr_sched)
-
-    print(curr_data)
-
-    curr_calendar = create_calendar(curr_data)
-    print(curr_calendar.serialize())
-
-    write_to_file(curr_calendar)
 
 if __name__ == "__main__":
     main()
